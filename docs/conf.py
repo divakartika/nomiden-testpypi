47007607c9fc7c0fe4babd1d0c146fe39d952b40
Diva Kartika <divakartika.larasati@gmail.com>
2023-02-14 16:08:19 +0700

    Add docs/conf.py

---
+++ docs/conf.py
@@ -0,0 +1,27 @@
+# Configuration file for the Sphinx documentation builder.
+
+# -- Project information
+
+project = 'nomiden'
+copyright = '2023, Diva K'
+author = 'Diva K'
+
+# -- General configuration
+
+extensions = [
+    'sphinx.ext.duration',
+    'sphinx.ext.doctest',
+    'sphinx.ext.autodoc',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.intersphinx',
+    'myst_parser'
+]
+
+autoapi_dirs = ["../nomiden"]
+
+# -- Options for HTML output
+
+html_theme = 'sphinx_rtd_theme'
+
+# -- Options for EPUB output
+epub_show_urls = 'footnote'
