2d950248f04b156615c48860c86713071c3b4450
Diva Kartika <divakartika.larasati@gmail.com>
2023-02-15 10:15:09 +0700

    Add autoapi_ignore to conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -24,6 +24,7 @@ autoapi_dirs = ["../nomiden"]
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = ["*test*", "*tests*", "nik.py", "kk.py"]
+autoapi_ignore = ["*test*"]
 
 # -- Options for HTML output
 
